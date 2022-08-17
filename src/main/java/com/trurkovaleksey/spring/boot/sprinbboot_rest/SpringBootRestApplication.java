23a5cc46d453db7d98bff37a6898e43e7fe1e290
Aleksey Turkov <alextur94@gmail.com>
2022-08-17 16:11:23 +0300

    fix bug

--- src/main/java/com/trurkovaleksey/spring/boot/sprinbboot_rest/SpringBootRestApplication.java
+++ src/main/java/com/trurkovaleksey/spring/boot/sprinbboot_rest/SpringBootRestApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SprinbbootRestApplication {
+public class SpringBootRestApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(SprinbbootRestApplication.class, args);
+		SpringApplication.run(SpringBootRestApplication.class, args);
 	}
 }
--- src/test/java/com/trurkovaleksey/spring/boot/sprinbboot_rest/SpringBootRestApplicationTests.java
+++ src/test/java/com/trurkovaleksey/spring/boot/sprinbboot_rest/SpringBootRestApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SprinbbootRestApplicationTests {
+class SpringBootRestApplicationTests {
 
 	@Test
 	void contextLoads() {
