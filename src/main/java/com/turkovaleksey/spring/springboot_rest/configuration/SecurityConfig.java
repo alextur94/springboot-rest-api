adabb165cf68f9e0ae0a59526a58d047a2f256d8
Aleksey Turkov <alextur94@gmail.com>
2022-09-27 16:22:35 +0300

    fix bug cors 3

--- src/main/java/com/turkovaleksey/spring/springboot_rest/configuration/SecurityConfig.java
+++ src/main/java/com/turkovaleksey/spring/springboot_rest/configuration/SecurityConfig.java
@@ -9,11 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Bean
-    CorsFilter corsFilter() {
-        CorsFilter filter = new CorsFilter();
-        return filter;
-    }
+//    @Bean
+//    CorsFilter corsFilter() {
+//        CorsFilter filter = new CorsFilter();
+//        return filter;
+//    }
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/**").permitAll();
