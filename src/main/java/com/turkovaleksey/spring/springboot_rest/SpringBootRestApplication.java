08838002f8ddd67880dfc76686a137bc955eb53f
Aleksey Turkov <alextur94@gmail.com>
2022-09-27 19:14:08 +0300

    fix

--- src/main/java/com/turkovaleksey/spring/springboot_rest/SpringBootRestApplication.java
+++ src/main/java/com/turkovaleksey/spring/springboot_rest/SpringBootRestApplication.java
@@ -2,9 +2,6 @@ package com.turkovaleksey.spring.springboot_rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringBootRestApplication {
@@ -12,14 +9,14 @@ public class SpringBootRestApplication {
 		SpringApplication.run(SpringBootRestApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfig() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000");
-			}
-		};
-	}
+//	@Bean
+//	public WebMvcConfigurer corsConfig() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**")
+//						.allowedOrigins("http://localhost:3000");
+//			}
+//		};
+//	}
 }
