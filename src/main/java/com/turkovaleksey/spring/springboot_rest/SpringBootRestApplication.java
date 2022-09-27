11da48cdf38e73732a5b8315fa822b0723f6dfb1
Aleksey Turkov <alextur94@gmail.com>
2022-09-27 18:56:39 +0300

    add global cors

--- src/main/java/com/turkovaleksey/spring/springboot_rest/SpringBootRestApplication.java
+++ src/main/java/com/turkovaleksey/spring/springboot_rest/SpringBootRestApplication.java
@@ -12,13 +12,14 @@ public class SpringBootRestApplication {
 		SpringApplication.run(SpringBootRestApplication.class, args);
 	}
 
-//	@Bean
-//	public WebMvcConfigurer corsConfig() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/**");
-//			}
-//		};
-//	}
+	@Bean
+	public WebMvcConfigurer corsConfig() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
--- src/main/java/com/turkovaleksey/spring/springboot_rest/controller/MyRestController.java
+++ src/main/java/com/turkovaleksey/spring/springboot_rest/controller/MyRestController.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
-@CrossOrigin
 public class MyRestController {
 
     @Autowired
