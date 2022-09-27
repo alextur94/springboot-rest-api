bebbd20976424af884eb5bfa5c645626bfad31fa
Aleksey Turkov <alextur94@gmail.com>
2022-09-27 18:49:01 +0300

    fix cors

--- src/main/java/com/turkovaleksey/spring/springboot_rest/SpringBootRestApplication.java
+++ src/main/java/com/turkovaleksey/spring/springboot_rest/SpringBootRestApplication.java
@@ -2,10 +2,23 @@ package com.turkovaleksey.spring.springboot_rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringBootRestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfig() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 }
