d1be8fed8e89241c175f42eb65ec1ec10a459c3e
Aleksey Turkov <alextur94@gmail.com>
2022-09-29 19:13:04 +0300

    change impl cors

--- src/main/java/com/turkovaleksey/spring/springboot_rest/SpringBootRestApplication.java
+++ src/main/java/com/turkovaleksey/spring/springboot_rest/SpringBootRestApplication.java
@@ -2,6 +2,9 @@ package com.turkovaleksey.spring.springboot_rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringBootRestApplication {
@@ -9,14 +12,14 @@ public class SpringBootRestApplication {
 		SpringApplication.run(SpringBootRestApplication.class, args);
 	}
 
-//	@Bean
-//	public WebMvcConfigurer corsConfig() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/**")
-//						.allowedOrigins("http://localhost:3000");
-//			}
-//		};
-//	}
+	@Bean
+	public WebMvcConfigurer corsConfig() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
