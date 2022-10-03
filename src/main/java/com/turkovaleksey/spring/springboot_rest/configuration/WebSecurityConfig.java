a2a9ddd559aa9bee16adc22b5618a7350a73f5d6
Aleksey Turkov <alextur94@gmail.com>
2022-10-03 08:35:43 +0300

    change host

--- src/main/java/com/turkovaleksey/spring/springboot_rest/configuration/WebSecurityConfig.java
+++ src/main/java/com/turkovaleksey/spring/springboot_rest/configuration/WebSecurityConfig.java
@@ -30,7 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3001/"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
