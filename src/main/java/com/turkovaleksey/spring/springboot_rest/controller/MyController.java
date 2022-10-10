605e88221c97e62d793140dc3297e8527d6dafba
Aleksey Turkov <alextur94@gmail.com>
2022-10-10 15:28:45 +0300

    conf

--- src/main/java/com/turkovaleksey/spring/springboot_rest/controller/MyController.java
+++ src/main/java/com/turkovaleksey/spring/springboot_rest/controller/MyController.java
@@ -3,13 +3,21 @@ package com.turkovaleksey.spring.springboot_rest.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 
 @Controller
 public class MyController {
 
     @GetMapping("/admin")
-    public String getAdminPage() {
-        return "forward:/localhost:3000/admin";
+    public String getAdminPage(HttpServletResponse response) {
+        try {
+            response.sendRedirect("http://localhost:3000/admin");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
     }
 
 }
