76281c4db596183b689cdcac8ffb8a0b93a50043
Aleksey Turkov <alextur94@gmail.com>
2022-10-15 19:57:30 +0300

    asd

--- src/main/java/com/turkovaleksey/spring/springboot_rest/configuration/SecurityConfig.java
+++ src/main/java/com/turkovaleksey/spring/springboot_rest/configuration/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.DELETE,"/api/**").hasRole("ADMIN")
                 .and()
                 .formLogin()
-//                .loginPage("http://localhost:3000/login")
+                .loginPage("http://localhost:3000/login")
                 .defaultSuccessUrl("http://localhost:3000/")
                 .and()
                 .logout()
