6ada1c9e1d16dfdc1baf478311aa5d2c45e811c1
Aleksey Turkov <alextur94@gmail.com>
2022-10-15 20:23:15 +0300

    add custom form my page

--- src/main/java/com/turkovaleksey/spring/springboot_rest/controller/MyController.java
+++ src/main/java/com/turkovaleksey/spring/springboot_rest/controller/MyController.java
@@ -7,20 +7,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.security.Principal;
 
 @Controller
 public class MyController {
     @GetMapping("/admin")
-    public String getAdminPage(HttpServletRequest request, HttpServletResponse response) {
+    public String getAdminPage(HttpServletRequest request, HttpServletResponse response, Principal principal) {
+        String name = principal.getName();
         try {
             HttpSession session = request.getSession(true);
-            session.setAttribute("access", "y");
+            session.setAttribute("username", name);
             response.sendRedirect("http://localhost:3000/admin");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-//        HttpSession session = request.getSession(true);
-//        session.setAttribute("mySessionAttribute", "someValue");
+        return null;
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage() {
         return "test";
     }
 
