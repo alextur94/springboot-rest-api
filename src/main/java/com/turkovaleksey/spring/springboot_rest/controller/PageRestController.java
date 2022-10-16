b67cc824b9748ab8598015193a23be7a6256c76c
Aleksey Turkov <alextur94@gmail.com>
2022-10-16 16:25:35 +0300

    add info actul user

--- src/main/java/com/turkovaleksey/spring/springboot_rest/controller/PageRestController.java
+++ src/main/java/com/turkovaleksey/spring/springboot_rest/controller/PageRestController.java
@@ -1,8 +1,16 @@
 package com.turkovaleksey.spring.springboot_rest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class PageRestController {
+import java.security.Principal;
 
+@RestController
+@RequestMapping("/info")
+public class PageRestController {
+    @GetMapping("/actual_username")
+    public String getActualUser(Principal principal) {
+        return principal.getName();
+    }
 }
